bc1f395c884e861df2e2db5cd042b48a869675b0
Andrei Ciucle <andrei.ciucle02@gmail.com>
2022-05-24 19:56:00 +0300

    Add divide to switch statement and change stack from int to double

--- Calculator.cs
+++ Calculator.cs
@@ -14,19 +14,19 @@ namespace CalculatorApp
 
             string[] temp = input.Split(' ');
 
-            Stack<int> stack = new Stack<int>();
+            Stack<double> stack = new Stack<double>();
 
             foreach (string s in temp)
             {
                 if (s[0] > '0' && s[0] <= '9')
                 {
-                    int t = int.Parse(s);
+                    double t = double.Parse(s);
                     stack.Push(t);
                 }
                 else
                 {
-                    int t1 = stack.Pop();
-                    int t2 = stack.Pop();
+                    double t1 = stack.Pop();
+                    double t2 = stack.Pop();
 
                     switch(s[0])
                     {
@@ -35,6 +35,8 @@ namespace CalculatorApp
                         case '-': stack.Push(t1 - t2);
                             break;
                         case '*': stack.Push(t1 * t2);
+                            break;
+                        case '/': stack.Push(t1 / t2);
                             break;
                     }
                 }
